319ef09f3d863104756388b0725a10ed3f70edf6
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-03-05 15:42:52 +0300

    add login carcass

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,26 @@
 import { ReactElement } from 'react';
 
-export const Login = (): ReactElement => (
-  <div>
-    <h1>Login</h1>
-  </div>
-);
+import { SuperButton, SuperInputText } from '../components';
+
+export const Login = (): ReactElement => {
+  console.log('asd');
+
+  const {} = useForm();
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          login:
+          <SuperInputText name="login" type="text" />
+        </div>
+        <div>
+          password:
+          <SuperInputText name="password" type="password" />
+        </div>
+        <SuperButton type="submit">login</SuperButton>
+      </form>
+    </div>
+  );
+};
