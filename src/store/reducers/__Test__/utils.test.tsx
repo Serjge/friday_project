b407a558719285c3d2f4a7d2894f8b0a80ba93c8
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-03-11 13:55:54 +0300

    add test for error validator

---
+++ src/store/reducers/__Test__/utils.test.tsx
@@ -0,0 +1,16 @@
+import { ErrorValidateMessage } from 'enum/errorValidateMessage';
+import { getErrorValidate } from 'utils';
+
+test('error validate', () => {
+  const result1 = getErrorValidate('required');
+  const result2 = getErrorValidate('minLength');
+  const result3 = getErrorValidate('validate');
+  const result4 = getErrorValidate('pattern');
+  const result5 = getErrorValidate('some text');
+
+  expect(result1).toBe(ErrorValidateMessage.required);
+  expect(result2).toBe(ErrorValidateMessage.minLength);
+  expect(result3).toBe(ErrorValidateMessage.validate);
+  expect(result4).toBe(ErrorValidateMessage.pattern);
+  expect(result5).toBe(undefined);
+});
--- src/utils/getErrorValidate.ts
+++ src/utils/getErrorValidate.ts
@@ -1,5 +1,7 @@
 import { UndefinedAnd } from 'types';
 
+// export type GetErrorValidate = 'required' | 'minLength' | 'validate' | 'pattern'; // оставил, чтобы куда-нибудь добавить. Или здесь или в общий тип отправить.
+
 export const getErrorValidate = (type: UndefinedAnd<string>): UndefinedAnd<string> => {
   switch (type) {
     case 'required':
