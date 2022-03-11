62b0a85fe325488a8354513fa08206368b403e77
Serjge <serjge@yandex.ru>
2022-03-11 11:19:24 +0300

    fixed bugs first loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { ReactElement, useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import { Header, Loading } from 'components';
+import { PATH } from 'enum';
 import { Routers } from 'pages';
-import { selectIsInitialize } from 'store/selectors';
+import { selectIsInitialize, selectIsLogin } from 'store/selectors';
 import { authMeTC } from 'store/thunks/profileThunks';
 import { Wrapper } from 'styles';
 
 export const App = (): ReactElement => {
   const dispatch = useDispatch();
-
+  const isLogin = useSelector(selectIsLogin);
+  const navigate = useNavigate();
   const isInitialize = useSelector(selectIsInitialize);
+
+  useEffect(() => {
+    dispatch(authMeTC());
+    if (!isLogin) {
+      navigate(PATH.LOGIN);
+    }
+  }, []);
 
   useEffect(() => {
     dispatch(authMeTC());
