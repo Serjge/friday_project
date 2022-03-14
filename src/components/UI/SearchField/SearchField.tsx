d82fa1fafdce646b44898ccf5523e5a3927b3661
Serjge <serjge@yandex.ru>
2022-03-14 12:04:05 +0300

    added SearchField.tsx

---
+++ src/components/UI/SearchField/SearchField.tsx
@@ -0,0 +1,26 @@
+import { memo, ReactElement, useCallback, useRef } from 'react';
+
+import { SuperButton, TextField } from 'components';
+
+type SearchFieldPropsType = {
+  searchValue: (value: string) => void;
+};
+
+export const SearchField = memo(({ searchValue }: SearchFieldPropsType): ReactElement => {
+  const valueRef = useRef<HTMLInputElement>(null);
+
+  const onSearchClick = useCallback((): void => {
+    const { value } = valueRef.current!;
+
+    if (value.trim() !== '') {
+      searchValue(value.trim());
+    }
+  }, []);
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <TextField ref={valueRef} />
+      <SuperButton onClick={onSearchClick}>Search</SuperButton>
+    </div>
+  );
+});
--- src/pages/PacksList.tsx
+++ src/pages/PacksList.tsx
@@ -1,8 +1,9 @@
-import { ReactElement, useEffect } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
 import { TableCardsPack } from 'components';
+import { SearchField } from 'components/UI/SearchField/SearchField';
 import { selectSortPacks } from 'store/selectors';
 import { getCardsTC } from 'store/thunks';
 
@@ -10,11 +11,22 @@ export const PacksList = (): ReactElement => {
   const dispatch = useDispatch();
 
   const sortPacks = useSelector(selectSortPacks);
+  const [valueInput, setValueInput] = useState('');
+
+  const searchValue = (value: string): void => {
+    setValueInput(value);
+  };
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-magic-numbers
     dispatch(getCardsTC('', 0, 0, sortPacks));
   }, [sortPacks]);
 
-  return <TableCardsPack />;
+  return (
+    <>
+      <SearchField searchValue={searchValue} />
+      <TableCardsPack />
+      {valueInput}
+    </>
+  );
 };
