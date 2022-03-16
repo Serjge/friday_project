4812ee9f9037dae08f1335e76ad6460d2f36ccce
DzmitryYesis <yesis1988@mail.ru>
2022-03-16 15:14:25 +0300

    Created thunk for sent new password.

--- src/store/thunks/passwordThunks.ts
+++ src/store/thunks/passwordThunks.ts
@@ -4,7 +4,7 @@ import { setErrorMessage } from '../actions';
 
 import { passwordApi } from 'api';
 import { statusCode } from 'enum';
-import { AppThunkType, ForgotPasswordSendType } from 'types';
+import { AppThunkType, ForgotPasswordSendType, SendNewPasswordType } from 'types';
 
 export const forgotPasswordTC =
   (data: ForgotPasswordSendType): AppThunkType =>
@@ -17,6 +17,24 @@ export const forgotPasswordTC =
       const status = response?.status;
 
       if (status === statusCode.Bad_Request) {
+        dispatch(setErrorMessage(error));
+      } else {
+        dispatch(setErrorMessage(message));
+      }
+    }
+  };
+
+export const SendNewPasswordTC =
+  (data: SendNewPasswordType): AppThunkType =>
+  async dispatch => {
+    try {
+      await passwordApi.sendNewPassword(data);
+    } catch (errorCatch) {
+      const { response, message } = errorCatch as AxiosError;
+      const error = response?.data.error;
+      const status = response?.status;
+
+      if (status === statusCode.Unauthorized || status === statusCode.Bad_Request) {
         dispatch(setErrorMessage(error));
       } else {
         dispatch(setErrorMessage(message));
