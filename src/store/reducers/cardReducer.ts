99933d4a96c2b6d40c0e8efbf8182b126bc1760b
Serjge <serjge@yandex.ru>
2022-03-16 11:45:14 +0300

    added case to cardReducer

--- src/store/reducers/cardReducer.ts
+++ src/store/reducers/cardReducer.ts
@@ -2,14 +2,14 @@ import { CardsType } from 'types';
 import { CardActionType } from 'types/actions';
 
 export type InitialStateType = {
-  packs: CardsType;
+  pack: CardsType;
   sort: string;
   searchAnswer: string;
   searchQuestion: string;
 };
 
 const initialState: InitialStateType = {
-  packs: {} as CardsType,
+  pack: {} as CardsType,
   sort: '',
   searchAnswer: '',
   searchQuestion: '',
@@ -20,6 +20,14 @@ export const cardReducer = (
   action: CardActionType,
 ): InitialStateType => {
   switch (action.type) {
+    case 'CARD/SET_CARD':
+      return { ...state, pack: action.card };
+    case 'CARD/SET_SORT_CARD':
+      return { ...state, sort: action.sort };
+    case 'CARD/SET_SEARCH_ANSWER':
+      return { ...state, searchAnswer: action.searchValue };
+    case 'CARD/SET_SEARCH_QUESTION':
+      return { ...state, searchQuestion: action.searchValue };
     default:
       return state;
   }
