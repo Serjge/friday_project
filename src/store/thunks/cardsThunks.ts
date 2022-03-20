6f08b857cfbaa6d716cf4ceed58df268bdf85692
Serjge <serjge@yandex.ru>
2022-03-20 21:49:13 +0300

    added deleteCardTC

--- src/store/thunks/cardsThunks.ts
+++ src/store/thunks/cardsThunks.ts
@@ -66,3 +66,25 @@ export const addCardTC =
       }
     }
   };
+
+export const deleteCardTC =
+  (cardId: string): AppThunkType =>
+  async dispatch => {
+    try {
+      const { status } = await cardsApi.deleteCard(cardId);
+
+      if (status === statusCode.OK) {
+        dispatch(rerenderCardAC());
+      }
+    } catch (errorCatch) {
+      const { response, message } = errorCatch as AxiosError;
+      const error = response?.data.error;
+      const status = response?.status;
+
+      if (status === statusCode.Bad_Request) {
+        dispatch(setErrorMessage(error));
+      } else {
+        dispatch(setErrorMessage(message));
+      }
+    }
+  };
