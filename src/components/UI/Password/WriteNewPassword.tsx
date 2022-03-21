911b23077dfb0187e6955c883fd01dd33bfd25a9
DzmitryYesis <yesis1988@mail.ru>
2022-03-21 14:37:20 +0300

    Created WriteNewPassword component.

---
+++ src/components/UI/Password/WriteNewPassword.tsx
@@ -0,0 +1,77 @@
+import { ReactElement } from 'react';
+
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { PATH } from '../../../enum';
+import { selectErrorMessage } from '../../../store/selectors';
+import { sendNewPasswordTC } from '../../../store/thunks';
+import { Wrapper } from '../../../styles';
+import { CreateNewPasswordType, SendNewPasswordType } from '../../../types';
+import { getErrorValidate } from '../../../utils';
+import { SuperButton } from '../SuperButton';
+import { TextField } from '../TextField';
+
+export const WriteNewPassword = (): ReactElement => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const param = useParams<'token'>();
+
+  const { token } = param;
+  const resetPasswordToken = String(token);
+
+  const errorMessage = useSelector(selectErrorMessage);
+
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: {
+      errors: { password, confirmPassword },
+    },
+  } = useForm<CreateNewPasswordType>();
+
+  const createNewPassword: SubmitHandler<CreateNewPasswordType> = data => {
+    const newPasswordData: SendNewPasswordType = {
+      password: data.password,
+      resetPasswordToken,
+    };
+    dispatch(sendNewPasswordTC(newPasswordData));
+    navigate(PATH.LOGIN);
+  };
+
+  return (
+    <Wrapper>
+      <h1>Create new password</h1>
+      <form onSubmit={handleSubmit(createNewPassword)}>
+        <TextField
+          {...register('password', { required: true, minLength: 8 })}
+          type="password"
+          labelTitle="New Password:"
+          placeholder="Password"
+          autoComplete="on"
+          error={getErrorValidate(password?.type)}
+        />
+        <TextField
+          labelTitle="Confirm password"
+          type="password"
+          {...register('confirmPassword', {
+            required: true,
+            minLength: 8,
+            validate: value => value === getValues('password'),
+          })}
+          placeholder="Confirm password"
+          error={getErrorValidate(confirmPassword?.type)}
+        />
+
+        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+          <SuperButton type="submit" value="create">
+            Create new password
+          </SuperButton>
+        </div>
+      </form>
+      {errorMessage || null}
+    </Wrapper>
+  );
+};
--- src/pages/Password/NewPassword.tsx
+++ src/pages/Password/NewPassword.tsx
@@ -1,76 +1,15 @@
 import { ReactElement } from 'react';
 
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-import { SuperButton, TextField } from 'components';
-import { PATH } from 'enum';
-import { selectErrorMessage } from 'store/selectors';
-import { sendNewPasswordTC } from 'store/thunks/passwordThunks';
-import { Wrapper } from 'styles';
-import { CreateNewPasswordType, SendNewPasswordType } from 'types';
-import { getErrorValidate } from 'utils';
+import { PasswordSuccessfulChanged } from '../../components/UI/Password/PasswordSuccesfulChanged';
+import { WriteNewPassword } from '../../components/UI/Password/WriteNewPassword';
+import { RootReducerType } from '../../store';
 
 export const NewPassword = (): ReactElement => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const param = useParams<'token'>();
-
-  const { token } = param;
-  const resetPasswordToken = String(token);
-
-  const errorMessage = useSelector(selectErrorMessage);
-
-  const {
-    register,
-    handleSubmit,
-    getValues,
-    formState: {
-      errors: { password, confirmPassword },
-    },
-  } = useForm<CreateNewPasswordType>();
-
-  const createNewPassword: SubmitHandler<CreateNewPasswordType> = data => {
-    const newPasswordData: SendNewPasswordType = {
-      password: data.password,
-      resetPasswordToken,
-    };
-    dispatch(sendNewPasswordTC(newPasswordData));
-    navigate(PATH.LOGIN);
-  };
-
-  return (
-    <Wrapper>
-      <h1>Create new password</h1>
-      <form onSubmit={handleSubmit(createNewPassword)}>
-        <TextField
-          {...register('password', { required: true, minLength: 8 })}
-          type="password"
-          labelTitle="New Password:"
-          placeholder="Password"
-          autoComplete="on"
-          error={getErrorValidate(password?.type)}
-        />
-        <TextField
-          labelTitle="Confirm password"
-          type="password"
-          {...register('confirmPassword', {
-            required: true,
-            minLength: 8,
-            validate: value => value === getValues('password'),
-          })}
-          placeholder="Confirm password"
-          error={getErrorValidate(confirmPassword?.type)}
-        />
-
-        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-          <SuperButton type="submit" value="create">
-            Create new password
-          </SuperButton>
-        </div>
-      </form>
-      {errorMessage || null}
-    </Wrapper>
+  const isChanged = useSelector<RootReducerType, boolean>(
+    state => state.password.isChange,
   );
+
+  return isChanged ? <WriteNewPassword /> : <PasswordSuccessfulChanged />;
 };
